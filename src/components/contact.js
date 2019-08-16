49375443938e9f8577e5d22856d5c15c64a2bab3
Elise Maschke <elise.maschke@outlook.de>
2019-08-16 17:24:01 +0200

    fake contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
+const faker = require('faker');
 
 class Contact extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.fakeMe = faker.helpers.userCard();
+  }
+
   render() {
     return (
-      <div>
+      <div className="contact">
         <h1>Contact page</h1>
+        {console.log(this.fakeMe)}
+        <p>Name: {this.fakeMe.name}</p>
+        <p>Address: {this.fakeMe.address.street} 101, {this.fakeMe.address.zipcode} {this.fakeMe.address.city}</p>
+        <p>Phone: {this.fakeMe.phone}</p>
+        <p>Email: {this.fakeMe.email}</p>
       </div>
     )
   }
