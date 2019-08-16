302f215772912b250681ecb480adc711b3fc29b1
Elise Maschke <elise.maschke@outlook.de>
2019-08-16 15:14:07 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// npm package: npm install --save react-mdl
+import "react-mdl/extra/material.css";
+import "react-mdl/extra/material.js";
+
+// npm package: npm install --save react-router-dom
+import { BrowserRouter} from "react-router-dom";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
